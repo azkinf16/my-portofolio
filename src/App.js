e9fefa4de877111bc293b22b3bc861127d6ca3b2
azkinf16 <azkifajri16@gmail.com>
2023-02-06 10:55:29 +0700

    add routing to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
     </div>
   );
 }
