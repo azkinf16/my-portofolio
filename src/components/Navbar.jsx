5fb81bdba50e60a93f00f11739872ba263463bfb
azkinf16 <azkifajri16@gmail.com>
2023-02-08 22:59:02 +0700

    change a bit styling and add useNavigate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div className="navbar">
-        <div className="flex items-center justify-between absolute pt-10 px-40 z-10 w-full">
-          <h1 className="text-2xl text-sky-200 cursor-default tracking-[5px] hover:tracking-[6px] duration-300">
-            <span className="font-black">Azki</span>Fajri
-          </h1>
-          <div className="flex justify-end w-full ml-12">
-            <h5 className="text-white text-lg tracking-wider cursor-pointer relative after:content-[''] after:bg-slate-400 after:h-[1.5px] after:w-0 after:left-0 after:-bottom-[1.5px] after:rounded-xl after:absolute after:duration-500 after:hover:w-full">
-              About
-            </h5>
-            <h5 className="text-white text-lg tracking-wider cursor-pointer relative after:content-[''] after:bg-slate-400 after:h-[1.5px] after:w-0 after:left-0 after:-bottom-[1.5px] after:rounded-xl after:absolute after:duration-500 after:hover:w-full ml-8">
-              Skills
-            </h5>
-            <h5 className="text-white text-lg tracking-wider cursor-pointer relative after:content-[''] after:bg-slate-400 after:h-[1.5px] after:w-0 after:left-0 after:-bottom-[1.5px] after:rounded-xl after:absolute after:duration-500 after:hover:w-full ml-8">
-              Projects
-            </h5>
-          </div>
-          <div className="ml-8">
-            <button className="bg-sky-200 py-2 px-3 rounded-full hover:scale-90 duration-300">Contact&nbsp;Me</button>
-          </div>
+      <nav className="flex absolute items-center justify-between pt-10 px-40 z-10 w-full">
+        <h1 className="text-2xl text-sky-200 cursor-default tracking-[5px] hover:tracking-[6px] duration-300">
+          <span className="font-black">Azki</span>Fajri
+        </h1>
+        <div className="flex justify-end w-full ml-12">
+          <p
+            onClick={() => navigate("/")}
+            className="text-white text-lg tracking-wider cursor-pointer relative after:content-[''] after:bg-slate-400 after:h-[1.5px] after:w-0 after:left-0 after:-bottom-[1.5px] after:rounded-xl after:absolute after:duration-500 after:hover:w-full"
+          >
+            About
+          </p>
+          <h5 className="text-white text-lg tracking-wider cursor-pointer relative after:content-[''] after:bg-slate-400 after:h-[1.5px] after:w-0 after:left-0 after:-bottom-[1.5px] after:rounded-xl after:absolute after:duration-500 after:hover:w-full ml-8">
+            Experiences
+          </h5>
+          <h5 className="text-white text-lg tracking-wider cursor-pointer relative after:content-[''] after:bg-slate-400 after:h-[1.5px] after:w-0 after:left-0 after:-bottom-[1.5px] after:rounded-xl after:absolute after:duration-500 after:hover:w-full ml-8">
+            Projects
+          </h5>
         </div>
-      </div>
+        <div className="ml-8">
+          <button className="bg-gradient-to-b from-[#1b1b31] to-[#22222d] py-2 px-3 font-bold rounded-full hover:scale-90 duration-300 text-white">
+            Contact&nbsp;Me
+          </button>
+        </div>
+      </nav>
     </>
   );
 }
